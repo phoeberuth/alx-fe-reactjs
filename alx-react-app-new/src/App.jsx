eda603c73326698a8d90ab7c7c35fff6e8d60abb
phoeberuth <phoeberuth.w@gmail.com>
2025-01-17 15:16:11 +0300

    Initial commit: Add core project files, including components, styles, and configuration files

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header'; // Import Header
+import MainContent from './components/MainContent'; // Import MainContent
+import Footer from './components/Footer'; // Import Footer
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+     {/* Displaying the WelcomeMessage component */}
+     <WelcomeMessage />
+
+     {/*Include Header, MainContent, and Footer components */}
+     <Header />
+     <MainContent />
+     <Footer />
+
+     {/* Add UserProfile Component with required props */}
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      
+      {/*Counter Application */}
+      <Counter />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
